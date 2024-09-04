ffdac651332fac04ce3717199de33c61c700a4e5
ominsi <kienenmulloy@gmail.com>
2024-09-04 04:52:01 -0400

    Created record to store statlines from a specific player during a specific year. Added list of the StatLines to use for storage.

--- Program.cs
+++ Program.cs
@@ -3,4 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+List<StatLines> stateLines = new List<StatLines>();
 app.Run();
+
+public record StatLines(int ID, string player, string team, int games, int atBats, int runs, int hits, int doubles, int triples, int homeRuns, int runsBattedIn, int stolenBases, int walks, float battingAverage, float slugging, float ops, float war, DateTime gameDate, int gameNumber = 0);
